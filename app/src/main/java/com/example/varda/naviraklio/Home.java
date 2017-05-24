6dc166dc9d185b73ee80011222d7a2ff3dff69e2
Endelehion <thanos-mortan@hotmail.com>
2017-05-25 02:34:17 +0300

    Calendar

--- app/src/main/java/com/example/varda/naviraklio/Home.java
+++ app/src/main/java/com/example/varda/naviraklio/Home.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 public class Home extends AppCompatActivity {
 
@@ -18,7 +17,7 @@ public class Home extends AppCompatActivity {
         startActivity(profileIntent);
     }
     protected void goCalendar(View view){
-        Intent profileIntent=new Intent(Home.this, Calendar.class);
+        Intent profileIntent=new Intent(Home.this, Organizer.class);
         startActivity(profileIntent);
     }
     protected void goMap(View view){
---
+++ app/src/main/java/com/example/varda/naviraklio/Organizer.java
@@ -0,0 +1,23 @@
+package com.example.varda.naviraklio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
+
+public class Organizer extends AppCompatActivity {
+    DatePicker datePicker;
+    final Calendar c = Calendar.getInstance();
+    int year = c.get(Calendar.YEAR);
+    int month = c.get(Calendar.MONTH);
+    int day = c.get(Calendar.DAY_OF_MONTH);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar);
+        datePicker = (DatePicker) findViewById(R.id.datePicker);
+        datePicker.updateDate(5, 5, 2016);
+    }
+}
