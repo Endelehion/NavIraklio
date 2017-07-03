c062e3364ff7e18b1d4790874eca8d7e80a589e7
Endelehion <thanos-mortan@hotmail.com>
2017-07-03 13:33:24 +0300

    Delete Appointments functionality one by one

--- app/src/main/java/com/example/varda/naviraklio/Appointment.java
+++ app/src/main/java/com/example/varda/naviraklio/Appointment.java
@@ -1,5 +1,7 @@
 package com.example.varda.naviraklio;
 
+import com.google.android.gms.maps.model.LatLng;
+
 import java.sql.Time;
 import java.util.Date;
 
@@ -11,9 +13,47 @@ public class Appointment {
 
     String type;
     String dateString;
+    LatLng coord;
+    int id;
 
-    public Appointment(String type, String dateString){
+
+    public Appointment(int id, String type, String dateString, double lat, double lon){
         this.type=type;
         this.dateString=dateString;
+        this.coord=new LatLng(lat,lon);
+        this.id=id;
     }
+
+    public String getDateString() {
+        return dateString;
+    }
+
+    public void setDateString(String dateString) {
+        this.dateString = dateString;
+    }
+
+    public LatLng getCoord() {
+        return coord;
+    }
+
+    public void setCoord(LatLng coord) {
+        this.coord = coord;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
 }
--- app/src/main/java/com/example/varda/naviraklio/Organizer.java
+++ app/src/main/java/com/example/varda/naviraklio/Organizer.java
@@ -3,9 +3,14 @@ package com.example.varda.naviraklio;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.util.SparseBooleanArray;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -14,57 +19,80 @@ import java.util.Calendar;
 import java.util.Date;
 
 public class Organizer extends AppCompatActivity {
-    DatePicker datePicker;
-    SimpleDateFormat sdateFormat;
-    final Calendar c = Calendar.getInstance();
-    int year = c.get(Calendar.YEAR);
-    int month = c.get(Calendar.MONTH);
-    int day = c.get(Calendar.DAY_OF_MONTH);
-    ArrayList<Appointment> arListAp;
-    ArrayAdapter adapterAp;
-    ListView listViewAp;
-    String dateString;
+    private DatePicker datePicker;
+    private SimpleDateFormat sdateFormat;
+    private final Calendar calendar = Calendar.getInstance();
+    private int year, month, day;
+    private Button newAp, delAp;
+    private ArrayList<Appointment> arListAp;
+    private ArrayAdapter adapterAp;
+    private ListView listViewAp;
+    private String dateString;
+    ArrayList<String> stringList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
         datePicker = (DatePicker) findViewById(R.id.datePicker);
+
+        year = calendar.get(Calendar.YEAR);
+        month = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
         datePicker.updateDate(year, month, day);
-        sdateFormat= new SimpleDateFormat("EEE d MMM yyyy HH:mm");
+        sdateFormat = new SimpleDateFormat("EEE d MMM yyyy HH:mm");
 
         arListAp = new ArrayList<>();
-        Date myDate;
-        Calendar cal = Calendar.getInstance();
-        cal.set(Calendar.MONTH, 9);
-        cal.set(Calendar.DATE, 24);
-        cal.set(Calendar.YEAR, 2013);
-        cal.set(Calendar.HOUR, 13);
-        cal.set(Calendar.MINUTE, 45);
-        cal.set(Calendar.SECOND, 52);
-        myDate = cal.getTime();
-        dateString=sdateFormat.format(myDate);
-        arListAp.add(new Appointment("Cinema", dateString));
-        cal.set(Calendar.MONTH, 4);
-        cal.set(Calendar.DATE, 23);
-        cal.set(Calendar.YEAR, 2017);
-        cal.set(Calendar.HOUR, 12);
-        cal.set(Calendar.MINUTE, 41);
-        cal.set(Calendar.SECOND, 59);
-        myDate = cal.getTime();
 
-        dateString=sdateFormat.format(myDate);
-        arListAp.add(new Appointment("Gas", dateString));
+        dateString = sdateFormat.format(createDate(4, 5, 2017, 15, 24));
+        arListAp.add(new Appointment(1, "Cinema", dateString, 37.977817, 23.769849));
 
-        ArrayList<String> stringList= new ArrayList<>();
+        dateString = sdateFormat.format(createDate(7, 7, 2017, 12, 55));
+        arListAp.add(new Appointment(2, "Gas", dateString, 37.977817, 23.769849));
+
+        stringList = new ArrayList<>();
         for (int i = 0; i < arListAp.size(); i++) {
-            stringList.add(arListAp.get(i).type+" "+arListAp.get(i).dateString);
+            stringList.add(arListAp.get(i).type + " " + arListAp.get(i).dateString);
         }
 
-        adapterAp = new ArrayAdapter<>(Organizer.this, android.R.layout.simple_list_item_1, stringList);
-        listViewAp=(ListView) findViewById(R.id.listAp);
+        adapterAp = new ArrayAdapter<>(Organizer.this, android.R.layout.simple_list_item_single_choice, stringList);
+        listViewAp = (ListView) findViewById(R.id.listAp);
         listViewAp.setAdapter(adapterAp);
+        listViewAp.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+
+
+        newAp = (Button) findViewById(R.id.newAp);
+        delAp = (Button) findViewById(R.id.delAp);
+
+        delAp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int adapterIndex;
+                adapterIndex = listViewAp.getCheckedItemPosition();
+                if (adapterIndex != -1) {
+                    arListAp.remove(adapterIndex);
+                    stringList.remove(adapterIndex);
+                    adapterAp.notifyDataSetChanged();
+                    listViewAp.clearChoices();
+
+                } else {
+                    Toast.makeText(Organizer.this, "No Appointments Selected", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 
+    Date createDate(int date, int month, int year, int hour, int minute) {
+        Date myDate;
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.MONTH, month);
+        cal.set(Calendar.DATE, date);
+        cal.set(Calendar.YEAR, year);
+        cal.set(Calendar.HOUR, hour);
+        cal.set(Calendar.MINUTE, minute);
+        myDate = cal.getTime();
+        return myDate;
+    }
 
 }
